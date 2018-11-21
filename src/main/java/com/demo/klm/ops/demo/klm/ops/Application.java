e6acdca52b1aac916aa945aeb47aec04ea14450c
Venkatapathy <1565359@zone1.scb.net>
2018-11-21 21:17:27 +0530

    SecurityCheck

--- src/main/java/com/demo/klm/ops/demo/klm/ops/Application.java
+++ src/main/java/com/demo/klm/ops/demo/klm/ops/Application.java
@@ -8,12 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 import com.demo.klm.ops.demo.klm.ops.h2.dao.StockPriceJdbcRepository;
 
 @SpringBootApplication
-@EnableOAuth2Sso
 public class Application implements CommandLineRunner{
 	
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
